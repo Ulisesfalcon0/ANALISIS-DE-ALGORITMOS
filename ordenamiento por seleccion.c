5b2212bbb5f7aadac09e0b678062b8192470dd4b
Ulisesfalcon0 <113311476+Ulisesfalcon0@users.noreply.github.com>
2022-09-22 21:43:26 -0500

    Add files via upload

---
+++ ordenamiento por seleccion.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+	int num []={43, 32, 45, 54, 21};
+	int aux, min;
+	
+	for(int i = 0 ;i < 5; i++){
+		min = i;
+		for(int j = i+1; j < 5; j++){
+			if (num[j] < num[min]){
+				min = j;
+			}
+		}
+		aux = num[i];
+		num[i] = num[min];
+		num[min] = aux;
+		
+	}
+	
+	printf("El orden es: ");
+	for(int i = 0 ;i < 5; i++){
+		printf("%i ", num[i]);
+	}
+}
+
